81a4b62774d224c9cc26caa97f04809d339034a0
Andrey <anmokretsov@gmail.com>
2020-05-31 18:59:11 +0300

    move routing to a separate file

---
+++ cmd/service-api/handler/list.go
@@ -0,0 +1,91 @@
+package handler
+
+import (
+	"github.com/A-ndrey/tododo/internal/list"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+type Handler struct {
+	ListService list.Service
+}
+
+func RouteList(apiGroup *gin.RouterGroup, handler *Handler) {
+	listApi := apiGroup.Group("/list")
+
+	listApi.GET("/:id", handler.GetItem)
+
+	listApi.POST("/", handler.CreateItem)
+
+	listApi.PUT("/", handler.UpdateItem)
+
+	listApi.DELETE("/:id", handler.DeleteItem)
+}
+
+func (h *Handler) GetItem(context *gin.Context) {
+	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.Status(http.StatusBadRequest)
+		return
+	}
+
+	i, err := h.ListService.GetItem(id)
+	if err != nil {
+		context.Status(http.StatusInternalServerError) // todo error type
+		return
+	}
+
+	context.JSON(http.StatusOK, i)
+}
+
+func (h *Handler) CreateItem(context *gin.Context) {
+	var i list.Item
+
+	if err := context.BindJSON(&i); err != nil {
+		log.Println(err)
+		return
+	}
+
+	err := h.ListService.AddNewItem(i)
+	if err != nil {
+		context.Status(http.StatusInternalServerError)
+		return
+	}
+
+	context.Status(http.StatusCreated)
+}
+
+func (h *Handler) UpdateItem(context *gin.Context) {
+	var i list.Item
+
+	if err := context.BindJSON(&i); err != nil {
+		log.Println(err)
+		return
+	}
+
+	err := h.ListService.UpdateItem(i)
+	if err != nil {
+		context.Status(http.StatusInternalServerError)
+		return
+	}
+
+	context.Status(http.StatusOK)
+}
+
+func (h *Handler) DeleteItem(context *gin.Context) {
+	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.Status(http.StatusBadRequest)
+		return
+	}
+
+	err = h.ListService.DeleteItem(id)
+	if err != nil {
+		context.Status(http.StatusInternalServerError) // todo error type
+		return
+	}
+
+	context.Status(http.StatusOK)
+}
--- cmd/service-api/main.go
+++ cmd/service-api/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
+	"github.com/A-ndrey/tododo/cmd/service-api/handler"
 	"github.com/A-ndrey/tododo/internal/list"
 	"github.com/A-ndrey/tododo/internal/postgres"
 	"github.com/gin-gonic/gin"
 	"log"
-	"net/http"
-	"strconv"
 )
 
 func main() {
@@ -28,73 +27,8 @@ func main() {
 
 	api := r.Group("/api/v1")
 
-	listApi := api.Group("/list")
-
-	listApi.GET("/:id", func(context *gin.Context) {
-		id, err := strconv.ParseInt(context.Param("id"), 10, 64)
-		if err != nil {
-			context.Status(http.StatusBadRequest)
-			return
-		}
-
-		i, err := listService.GetItem(id)
-		if err != nil {
-			context.Status(http.StatusInternalServerError) // todo error type
-			return
-		}
-
-		context.JSON(http.StatusOK, i)
-	})
-
-	listApi.POST("/", func(context *gin.Context) {
-		var i list.Item
-
-		if err := context.BindJSON(&i); err != nil {
-			log.Println(err)
-			return
-		}
-
-		err := listService.AddNewItem(i)
-		if err != nil {
-			context.Status(http.StatusInternalServerError)
-			return
-		}
-
-		context.Status(http.StatusCreated)
-	})
-
-	listApi.PUT("/", func(context *gin.Context) {
-		var i list.Item
-
-		if err := context.BindJSON(&i); err != nil {
-			log.Println(err)
-			return
-		}
-
-		err := listService.UpdateItem(i)
-		if err != nil {
-			context.Status(http.StatusInternalServerError)
-			return
-		}
-
-		context.Status(http.StatusOK)
-	})
-
-	listApi.DELETE("/:id", func(context *gin.Context) {
-		id, err := strconv.ParseInt(context.Param("id"), 10, 64)
-		if err != nil {
-			context.Status(http.StatusBadRequest)
-			return
-		}
-
-		err = listService.DeleteItem(id)
-		if err != nil {
-			context.Status(http.StatusInternalServerError) // todo error type
-			return
-		}
-
-		context.Status(http.StatusOK)
-	})
+	listHandler := &handler.Handler{ListService: listService}
+	handler.RouteList(api, listHandler)
 
 	r.Run(":3000")
 }
